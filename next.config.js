9569fa01cae197eeb32404f3b4525d77438d6586
Miracl.in <aldrinwilfred.a@gmail.com>
2025-10-29 13:39:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/miraclin' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/miraclin' : ''
+  }
 }
 
 module.exports = nextConfig
