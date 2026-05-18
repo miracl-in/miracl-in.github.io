a9f1b82fd4ee9074495443545b319b82bd4c566c
Miraclin <aldrinwilfred.a@gmail.com>
2026-05-18 18:33:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon_Image.png" />
         <link rel="canonical" href="https://miracl.in" />
         <meta name="geo.region" content="IN-TN" />
         <meta name="geo.placename" content="Thanjavur" />
